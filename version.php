4cb9a07581666ca044e4912037e5d6115ea757b6
Glutamat42 <markus.heck13396@gmx.de>
2023-06-29 22:02:23 +0200

    update version.php

--- version.php
+++ version.php
@@ -7,11 +7,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023061600;
+$plugin->version = 2023062900;
 $plugin->requires = 2021051712.05;  // Moodle version
 $plugin->component = 'availability_adler';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '1.1.0_dev';
 $plugin->dependencies = array(
     'local_adler' => ANY_VERSION,   // The Foo activity must be present (any version).
 );
