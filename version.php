2c882898fcfc962d5df33362004db307566cd864
Markus Heck <markus.heck@hs-kempten.de>
2023-11-27 16:27:17 +0100

    Drop support for PHP version 7.4. Since mod_adleradaptivity already required php 8.0 the previous version wasn't actually supported by the AdLer project since using the adaptivity module. Only 8.1 is now supported.

--- version.php
+++ version.php
@@ -7,11 +7,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023111200;
+$plugin->version = 2023112701;
 $plugin->requires = 2022112800;  // Moodle version
 $plugin->component = 'availability_adler';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.0';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '3.0.0-dev';
 $plugin->dependencies = array(
     'local_adler' => '2.0.0',   // The Foo activity must be present (any version).
 );
