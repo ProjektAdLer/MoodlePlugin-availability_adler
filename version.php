6a030cdaf1339e69fc447d181ae2356a7a220162
Markus Heck <51326311+Glutamat42@users.noreply.github.com>
2025-05-13 14:58:34 +0200

    bump version for release

--- version.php
+++ version.php
@@ -10,7 +10,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2025031600;
 $plugin->requires = 2024042200;  // Moodle version
 $plugin->component = 'availability_adler';
-$plugin->release = '4.1.0';
+$plugin->release = '4.2.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_logging' => ANY_VERSION,
